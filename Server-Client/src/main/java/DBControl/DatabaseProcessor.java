bd7fd567d04ac66e3f91bce63e3ab1084278a70a
DmitriyV <dima1999-91@mail.ru>
2024-08-03 00:46:07 +0300

    DB 0001

--- Server-Client/src/main/java/DBControl/DatabaseProcessor.java
+++ Server-Client/src/main/java/DBControl/DatabaseProcessor.java
@@ -1,4 +1,15 @@
 package DBControl;
+import DBControl.SqLite.Connect;
+
+import java.sql.SQLException;
 
 public class DatabaseProcessor {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connect.Conn();
+        Connect.CreateDB();
+        Connect.WriteDB();
+        Connect.ReadDB();
+        Connect.CloseDB();
+    }
 }
